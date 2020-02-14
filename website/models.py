30727798bb92ca2b110ea8dd7ba4a3f154810996
jsmithdataanalytics <jsmithdataanalytics@gmail.com>
2020-02-14 23:02:54 +0000

    Added Pokemon and Type models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Pokemon(models.Model):
+    number = models.IntegerField(unique=True)
+    name = models.CharField(max_length=15, unique=True)
+    primary_type = models.ForeignKey(Type, related_name='primary', on_delete=models.CASCADE)
+    secondary_type = models.ForeignKey(Type, related_name='secondary', on_delete=models.CASCADE, null=True, blank=True)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    genus = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    image = models.CharField(max_length=10, unique=True)
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special = models.IntegerField()
+    speed = models.IntegerField()
