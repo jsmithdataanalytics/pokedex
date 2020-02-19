4acb7810cb6494f53638107320ebf7173de8176b
jsmithdataanalytics <jsmithdataanalytics@gmail.com>
2020-02-19 16:49:20 +0000

    Improved profile page, removed login buttons

--- website/models.py
+++ website/models.py
@@ -27,5 +27,20 @@ class Pokemon(models.Model):
     def types(self):
         return [self.primary_type, self.secondary_type]
 
+    def height_in_m(self):
+        return self.height / 10.0
+
+    def height_in_in(self):
+        return self.height_in_m() / 0.0254
+
+    def height_in_ft(self):
+        return self.height_in_in() // 12, self.height_in_in() % 12
+
+    def weight_in_kg(self):
+        return self.weight / 10.0
+
+    def weight_in_lb(self):
+        return self.weight_in_kg() * 2.2
+
     def __str__(self):
         return f'Pokemon({self.name})'
